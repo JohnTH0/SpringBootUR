202282cbf4c394533c80cd34ec5cf31188f19e58
TH <ok821000@gmail.com>
2025-01-02 23:27:46 +0900

    [Modify] GET Method 테스트 - 생성자에 대한 개념 파악 및 Get Method 생성 후 확인

--- src/main/java/com/example/rest_demo/RestDemoApplication.java
+++ src/main/java/com/example/rest_demo/RestDemoApplication.java
@@ -2,6 +2,13 @@ package com.example.rest_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class RestDemoApplication {
@@ -11,3 +18,49 @@ public class RestDemoApplication {
 	}
 
 }
+
+class Coffee {
+	private final String id; // 한번 생성한 이후 수정할 일이 없으므로 final 선언
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name){
+		this(UUID.randomUUID().toString(), name);
+	}
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+@RestController
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+				new Coffee("Ice Americano"),
+				new Coffee("Ice Latte"),
+				new Coffee("Ice Capuchin"),
+				new Coffee("Ice Hazelnut")
+		));
+	}
+
+	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+}
+
