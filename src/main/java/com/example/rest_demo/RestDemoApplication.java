006ec38f4fe87acc8fbba233844ec9525ee39792
TH <ok821000@gmail.com>
2025-01-04 23:54:29 +0900

    [Modify] CRUD 수정 - RequestMapping 을 사용하여 공통된 url 처리 - PUT method logic 수정

--- src/main/java/com/example/rest_demo/RestDemoApplication.java
+++ src/main/java/com/example/rest_demo/RestDemoApplication.java
@@ -2,10 +2,13 @@ package com.example.rest_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -44,6 +47,7 @@ class Coffee {
 }
 
 @RestController
+@RequestMapping("/coffees")
 class RestApiDemoController {
 	private List<Coffee> coffees = new ArrayList<>();
 
@@ -56,19 +60,30 @@ class RestApiDemoController {
 		));
 	}
 
-	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+	@GetMapping
 	Iterable<Coffee> getCoffees() {
 		return coffees;
 	}
 
-	@PostMapping("/coffees")
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee coffee : coffees) {
+			if(coffee.getId().equals(id)) {
+				return Optional.of(coffee);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	@PostMapping
 	Coffee postCoffee (@RequestBody Coffee coffee){
 		coffees.add(coffee);
 		return coffee;
 	}
 
-	@PutMapping("/coffees/{id}")
-	Coffee putCoffee (@PathVariable String id, @RequestBody Coffee coffee){
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee (@PathVariable String id, @RequestBody Coffee coffee){
 		int coffeeIndex = -1;
 
 		for (Coffee c : coffees) {
@@ -78,10 +93,12 @@ class RestApiDemoController {
 			}
 		}
 
-		return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+		return (coffeeIndex == -1) ?
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+				new ResponseEntity<>(coffee, HttpStatus.OK);
 	}
 
-	@DeleteMapping("/coffees/{id}")
+	@DeleteMapping("/{id}")
 	void deleteCoffee (@PathVariable String id){
 		coffees.removeIf(c -> c.getId().equals(id));
 	}
